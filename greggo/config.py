ddc15d85b4068faddd75b0a64cb70099c6cf0766
dejicranium <itisdeji@gmail.com>
2019-02-15 09:48:29 +0100

    ewrwer

--- greggo/config.py
+++ greggo/config.py
@@ -2,7 +2,7 @@ import redis
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
-REDIS_SERVER = redis.Redis(host="pozeet-redis-cluster.0jy3so.ng.0001.use2.cache.amazonaws.com", port="6379")
-#REDIS_SERVER = redis.Redis()
+#REDIS_SERVER = redis.Redis(host="http://ec2-18-218-17-23.us-east-2.compute.amazonaws.com", port="6379")
+REDIS_SERVER = redis.Redis()
 def change_redis_connection():
     pass
